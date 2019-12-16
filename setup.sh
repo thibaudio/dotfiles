d202b0ebdc5c34ad25a300c14384083be336f397
Thibaud <thibaud.baas@spoon.ai>
2019-12-16 15:54:18 +0100

    Clean setup.sh

--- setup.sh
+++ setup.sh
@@ -2,12 +2,24 @@
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-if [ -z ${HOME} ]
-then
-	echo "ERROR: now \${HOME} env. variable found"
-	exit 1
+# Spacemacs
+function config_spacemacs() {
+    rm -f ${HOME}/.spacemacs
+    rm -f ${HOME}/.spacemacs.env
+
+    if [ -d ${HOME}/.spacemacs.d ] && [ -z ${FORCE} ]; then
+        echo "WARNING: Folder ${HOME}/.spacemacs.d already exist - Skipping"
+    else
+        rm -rf ${HOME}/.spacemacs.d
+        ln -sf $SCRIPTPATH/.spacemacs.d ${HOME}/.spacemacs.d
+    fi
+}
+
+if [ -z ${HOME} ] ;then
+	  echo "ERROR: now \${HOME} env. variable found"
+	  exit 1
 fi
 
-# Spacemacs
-rm -f ${HOME}/.spacemacs
-ln -sf $SCRIPTPATH/.spacemacs.d ${HOME}/.spacemacs.d
+[[ $1 -eq "-f" ]] && FORCE=true
+
+config_spacemacs
