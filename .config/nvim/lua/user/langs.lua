0c013e41d5d267f3d2673cf6ff1823e54f3913d1
Thibaud <1822780+thibaudio@users.noreply.github.com>
2023-09-26 12:07:28 +0200

    lsp-zero

--- .config/nvim/lua/user/langs.lua
+++ .config/nvim/lua/user/langs.lua
@@ -1,136 +1,42 @@
 local servers = {
-  rust_analyzer = {},
-	terraformls = {},
-	jedi_language_server = {},
-	lua_ls = {},
+  rust = 'rust_analyzer',
+	terraform = 'terraformls',
+	python = 'jedi_language_server',
+	lua = 'lua_ls',
+	gdscript = '',
+	help = '',
+	vim = '',
 }
 
--- nvim-cmp supports additional completion capabilities
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+local ts_servers = {}
+local lsp_servers = {}
 
--- Setup mason so it can manage external tooling
-require('mason').setup()
-
--- Ensure the servers above are installed
-local mason_lspconfig = require 'mason-lspconfig'
-
-mason_lspconfig.setup {
-  ensure_installed = vim.tbl_keys(servers),
-}
-
-mason_lspconfig.setup_handlers {
-  function(server_name)
-    require('lspconfig')[server_name].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-    }
-  end,
-}
-
--- Setup nvim-cmp.
-local cmp_status_ok, cmp = pcall(require, "cmp")
-if not cmp_status_ok then
-  return
+for k,v in pairs(servers) do
+  ts_servers[#ts_servers+1]=k
+	if v ~= '' then
+		lsp_servers[#lsp_servers+1] = v
+	end
 end
 
-cmp.setup({
-	snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-	mapping = {
-    ['<C-k>'] = cmp.mapping.select_prev_item(),
-    ['<C-j>'] = cmp.mapping.select_next_item(),
-    -- Add tab support
-    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-    ['<Tab>'] = cmp.mapping.select_next_item(),
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Insert,
-      select = true,
-    })
-  },
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'buffer' },
-    { name = 'luasnip' },
-  })
+vim.opt.signcolumn = 'yes'
+
+local lsp = require('lsp-zero').preset({
+  name = 'minimal',
+  set_lsp_keymaps = true,
+  manage_nvim_cmp = true,
+  suggest_lsp_servers = false,
 })
 
--- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline('/', {
-  sources = {
-    { name = 'buffer' }
-  }
-})
+lsp.ensure_installed(lsp_servers)
 
--- [[ Configure Treesitter ]]
--- See `:help nvim-treesitter`
+lsp.nvim_workspace()
+
+lsp.setup()
+
 require('nvim-treesitter.configs').setup {
-  -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'lua', 'python', 'rust', 'help', 'vim', 'terraform' },
+  ensure_installed = ts_servers,
 
   highlight = { enable = true },
-  indent = { enable = true, disable = { 'python' } },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = '<c-space>',
-      node_incremental = '<c-space>',
-      scope_incremental = '<c-s>',
-      node_decremental = '<c-backspace>',
-    },
-  },
-  textobjects = {
-    select = {
-      enable = true,
-      lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-      keymaps = {
-        -- You can use the capture groups defined in textobjects.scm
-        ['aa'] = '@parameter.outer',
-        ['ia'] = '@parameter.inner',
-        ['af'] = '@function.outer',
-        ['if'] = '@function.inner',
-        ['ac'] = '@class.outer',
-        ['ic'] = '@class.inner',
-      },
-    },
-    move = {
-      enable = true,
-      set_jumps = true, -- whether to set jumps in the jumplist
-      goto_next_start = {
-        [']m'] = '@function.outer',
-        [']]'] = '@class.outer',
-      },
-      goto_next_end = {
-        [']M'] = '@function.outer',
-        [']['] = '@class.outer',
-      },
-      goto_previous_start = {
-        ['[m'] = '@function.outer',
-        ['[['] = '@class.outer',
-      },
-      goto_previous_end = {
-        ['[M'] = '@function.outer',
-        ['[]'] = '@class.outer',
-      },
-    },
-    swap = {
-      enable = true,
-      swap_next = {
-        ['<leader>a'] = '@parameter.inner',
-      },
-      swap_previous = {
-        ['<leader>A'] = '@parameter.inner',
-      },
-    },
-  },
 }
 
 
--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -3,16 +3,16 @@ local fn = vim.fn
 -- Automatically install packer
 local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system {
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  }
-  print "Installing packer close and reopen Neovim..."
-  vim.cmd [[packadd packer.nvim]]
+	PACKER_BOOTSTRAP = fn.system {
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	}
+	print "Installing packer close and reopen Neovim..."
+	vim.cmd [[packadd packer.nvim]]
 end
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
@@ -26,81 +26,81 @@ vim.cmd [[
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-  return
+	return
 end
 
 -- Have packer use a popup window
 packer.init {
-  display = {
-    open_fn = function()
-      return require("packer.util").float { border = "rounded" }
-    end,
-  },
+	display = {
+		open_fn = function()
+			return require("packer.util").float { border = "rounded" }
+		end,
+	},
 }
 
 -- Install your plugins here
 return packer.startup(function(use)
-  -- My plugins here
-  use "wbthomason/packer.nvim" -- Have packer manage itself
+	-- My plugins here
+	use "wbthomason/packer.nvim" -- Have packer manage itself
+
+	-- Telescope
+	use {
+		'nvim-telescope/telescope.nvim',
+		requires = { { 'nvim-lua/plenary.nvim' } }
+	}
+
+	use { "nvim-telescope/telescope-file-browser.nvim" }
+
+	-- telekasten
+	use {
+		"renerocksai/telekasten.nvim",
+		requires = { { "nvim-telescope/telescope.nvim" } },
+	}
 
   -- LSP
-  use { -- LSP Configuration & Plugins
-    'neovim/nvim-lspconfig',
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v1.x',
     requires = {
-      -- Automatically install LSPs to stdpath for neovim
-      'williamboman/mason.nvim',
-      'williamboman/mason-lspconfig.nvim',
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},             -- Required
+      {'williamboman/mason.nvim'},           -- Optional
+      {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-      -- Useful status updates for LSP
-      'j-hui/fidget.nvim',
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},         -- Required
+      {'hrsh7th/cmp-nvim-lsp'},     -- Required
+      {'hrsh7th/cmp-buffer'},       -- Optional
+      {'hrsh7th/cmp-path'},         -- Optional
+      {'saadparwaiz1/cmp_luasnip'}, -- Optional
+      {'hrsh7th/cmp-nvim-lua'},     -- Optional
 
-      -- Additional lua configuration, makes nvim stuff amazing
-      'folke/neodev.nvim',
-    },
+      -- Snippets
+      {'L3MON4D3/LuaSnip'},             -- Required
+      {'rafamadriz/friendly-snippets'}, -- Optional
+    }
   }
-   -- Telescope
-   use {
-     'nvim-telescope/telescope.nvim',
-     requires = { {'nvim-lua/plenary.nvim'} }
-   } 
 
-	 use { "nvim-telescope/telescope-file-browser.nvim" }
- 
-   -- Treesitter
-   use {
-     "nvim-treesitter/nvim-treesitter",
-     run = ":TSUpdate",
-   }
+	-- Treesitter
+	use {
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+	}
 	use 'nvim-treesitter/nvim-treesitter-context'
 
-  use { -- Autocompletion
-    'hrsh7th/nvim-cmp',
-    requires = { 
-      'hrsh7th/cmp-nvim-lsp',
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',
-      'hrsh7th/cmp-buffer'
-    },
-  }
 
-  -- telekasten
-  use {
-    "renerocksai/telekasten.nvim",
-    requires = {{"nvim-telescope/telescope.nvim"}},
-  }
-
-  -- Which key
-  use "folke/which-key.nvim"
+	-- Which key
+	use "folke/which-key.nvim"
 
 	-- Theme
-  use { 
-    'dracula/vim',
-    config = function()
-      vim.cmd([[colorscheme dracula]])
-    end
-  }
+	use {
+		'dracula/vim',
+		config = function()
+			vim.cmd([[colorscheme dracula]])
+		end
+	}
 
-  use 'nvim-lualine/lualine.nvim'
+	use 'nvim-lualine/lualine.nvim'
 
 	-- Comments
 	use("JoosepAlviste/nvim-ts-context-commentstring")
@@ -111,13 +111,13 @@ return packer.startup(function(use)
 
 	-- git
 	use("tpope/vim-fugitive")
-	use({ "lewis6991/gitsigns.nvim"})
+	use({ "lewis6991/gitsigns.nvim" })
 
-	use({ "akinsho/toggleterm.nvim"})
+	use({ "akinsho/toggleterm.nvim" })
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if PACKER_BOOTSTRAP then
-    require("packer").sync()
-  end
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if PACKER_BOOTSTRAP then
+		require("packer").sync()
+	end
 end)
