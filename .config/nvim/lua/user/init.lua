5a70f0445d13d4beef8e9b412c8453d9756149ac
Thibaud <1822780+thibaudio@users.noreply.github.com>
2025-07-03 20:32:15 +0200

    Move to LazyVim

--- .config/nvim/lua/user/init.lua
+++
@@ -1,9 +0,0 @@
-require "user.options"
-require "user.plugins"
-require "user.mappings"
-require "user.whichkey"
-require "user.telescope"
-require "user.gitsigns"
-require "user.toggleterm"
-
-require("mason").setup()
