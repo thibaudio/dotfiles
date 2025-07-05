ab37857323439cbc0ecea3b0b808d61618376df1
Thibaud <1822780+thibaudio@users.noreply.github.com>
2025-07-05 14:57:39 +0200

    fix treesitter zig compile +gdscript

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -2,8 +2,32 @@ return {
   --- Treesitter
   {
     "nvim-treesitter/nvim-treesitter",
+    --- Copied From https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/plugins/treesitter.lua
+    --- To force zig as compiler
+    ---@param opts TSConfig
     config = function(_, opts)
       require("nvim-treesitter.install").compilers = { "zig" }
+      if type(opts.ensure_installed) == "table" then
+        opts.ensure_installed = LazyVim.dedup(opts.ensure_installed)
+      end
+      require("nvim-treesitter.configs").setup(opts)
     end,
+    opts = {
+      ensure_installed = {
+        "gdscript",
+        "godot_resource",
+        "gdshader",
+      },
+    },
+  },
+
+  --- LSP
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        gdscript = {},
+      },
+    },
   },
 }
