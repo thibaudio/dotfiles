6cb7acf2c450368beca072b8fe297ff1dc670d38
Thibaud <1822780+thibaudio@users.noreply.github.com>
2025-07-07 10:15:14 +0200

    dap config for godot

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -30,4 +30,36 @@ return {
       },
     },
   },
+
+  --- DAP
+  {
+    "mfussenegger/nvim-dap",
+
+    --- From https://github.com/LazyVim/LazyVim/discussions/894
+    dependencies = {
+      {
+        "jay-babu/mason-nvim-dap.nvim",
+        event = "BufReadPre",
+      },
+    },
+
+    --- From https://gist.github.com/tsraveling/166d3f0970c6a7217754d0619471d320
+    opts = function()
+      local dap = require("dap")
+      dap.adapters.godot = {
+        type = "server",
+        host = "127.0.0.1",
+        port = 6006,
+      }
+      dap.configurations.gdscript = {
+        {
+          type = "godot",
+          request = "launch",
+          name = "Launch scene",
+          project = "${workspaceFolder}",
+          launch_scene = true,
+        },
+      }
+    end,
+  },
 }
