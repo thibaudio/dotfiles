d8d914a9caec2b261d4266de2becd1911b80eae8
Thibaud <1822780+thibaudio@users.noreply.github.com>
2025-07-09 10:17:31 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -9,3 +9,4 @@ winget install sharkdp.fd
 
 # Link nvim config
 Start-Process powershell.exe -Verb runAs -ArgumentList "New-Item -Path $env:LOCALAPPDATA\nvim -ItemType SymbolicLink -Value $PSScriptRoot\.config\nvim -Force"
+Start-Process powershell.exe -Verb runAs -ArgumentList "New-Item -Path '$env:LOCALAPPDATA\nvim' -ItemType SymbolicLink -Value '$PSScriptRoot\.config\nvim' -Force"
